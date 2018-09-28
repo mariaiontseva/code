c0d62c48738032859b817256eb5006bd91725607
MARIA IONTSEVA <mariaiontseva@marias-mbp-3.home>
2018-09-28 13:22:25 +0100

    push?

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function setup() {
+    createCanvas (500, 500)
+    line (15, 20, 150, 20);
+}
+
+function draw () {
+    rect (100, 100, 50, 50);
+    stroke (144, 0,0);
+    fill (133,33,33)
+    
+
+}
+
