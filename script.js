d8d8ea280c9a0d229068b7498b20516d9f2c4935
MARIA IONTSEVA <mariaiontseva@marias-mbp-3.home>
2018-09-30 13:40:05 +0100

    upgrade of styles

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
 function setup() {
     createCanvas (500, 500)
-    line (15, 20, 150, 20);
+
 }
 
 function draw () {
     if (mouseIsPressed) {
             pixelDensity(5);
-            ellipse (100, 100, 50, 50);
+            ellipse (mouseX, mouseY, 50, 50);
             stroke (144, 0,0);
-            fill (188,11,11);
-            
+            fill (188,11,11);}
+    else {
+    pixelDensity(5);
+    rect (mouseX, mouseY, 50, 50);
+    stroke (144, 0,0);
+    fill (188,11,11);     
     }
     
         }
